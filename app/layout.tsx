f93ce5228d2c8a15aa3eec8e870dfea993e82835
moose-code <jonjonclark@gmail.com>
2025-02-13 19:42:29 +0000

    change default theme to light

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
