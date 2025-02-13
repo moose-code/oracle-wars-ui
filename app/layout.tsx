584015513f8a56566b148515b0d32fbcfd420e04
moose-code <jonjonclark@gmail.com>
2025-02-13 19:21:37 +0000

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { GeistSans, GeistMono } from "geist/font";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = GeistSans;
 const geistMono = GeistMono;
@@ -61,6 +62,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
